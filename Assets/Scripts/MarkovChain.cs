166097d8e2ff8a6d7edfc85753e80f0db6fda7e4
Kwok Yee Chu <k.y.chu96@gmail.com>
2018-10-07 23:42:16 +0100

    Added a managed plugin dll that prints the sum of two values

---
+++ Assets/Scripts/MarkovChain.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+
+public class MarkovChain : MonoBehaviour {
+
+    [DllImport("MarkovChain", EntryPoint = "Sum")]
+    public static extern double Sum(double a, double b);
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log(Sum(500, 100));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
