964563af798a2c8d23f66eeb3aaca947695e1578
Kwok Yee Chu <k.y.chu96@gmail.com>
2018-10-22 00:26:00 +0100

    manually controlling the weather states using 0 for Sunny, 1 for Raining and 2 for Windy

--- Assets/Scripts/MarkovChain.cs
+++ Assets/Scripts/MarkovChain.cs
@@ -6,16 +6,28 @@ using System.Runtime.InteropServices;
 
 public class MarkovChain : MonoBehaviour {
 
-    [DllImport("MarkovChain", EntryPoint = "Sum")]
-    public static extern double Sum(double a, double b);
+    [DllImport("MarkovChain", EntryPoint = "CalculateWeatherState")]
+    public static extern int CalculateWeatherState(int index);
 
-    // Use this for initialization
+    public int currentState = 0;
+
     void Start () {
-        Debug.Log(Sum(500, 100));
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        currentState = CalculateWeatherState(currentState);
+    }
+
+    void Update()
+    {
+        switch (currentState)
+        {
+            case 0:
+                Debug.Log("SUNNY");
+                break;
+            case 1:
+                Debug.Log("RAINING");
+                break;
+            case 2:
+                Debug.Log("WINDY");
+                break;
+        }
+    }
 }
