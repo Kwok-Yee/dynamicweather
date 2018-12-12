b824a6db47eb74abb0dafb9eabae985013b30e7a
Kwok Yee Chu <k.y.chu96@gmail.com>
2018-12-12 15:26:41 +0000

    updated code for demonstration

--- Assets/_Scripts/MarkovChainSystem.cs
+++ Assets/_Scripts/MarkovChainSystem.cs
@@ -7,45 +7,78 @@ public class MarkovChainSystem : MonoBehaviour
 {
     MarkovChain markovChain;
 
+    [SerializeField]
     private const int states = 4;
+    [SerializeField]
     private int currentState = 0;
-    private float startTime = 12;
-    private float repeatRateTime = 12;
+    [SerializeField]
+    private float waitTime = 5;
     private int runs = 0;
 
     private int[][] transitions = new int[states][];
     private double[][] probabilities = new double[states][];
+    private double[][] tempProbabilities = new double[states][];
 
+    [SerializeField]
     private int[] sunnyTrans = new int[states] { 0, 1, 2, 3 };
+    [SerializeField]
     private int[] cloudyTrans = new int[states] { 1, 0, 2, 3 };
+    [SerializeField]
     private int[] rainingTrans = new int[states] { 2, 0, 1, 3 };
+    [SerializeField]
     private int[] freezingTrans = new int[states] { 3, 0, 1, 2 };
 
+    [SerializeField]
     private double[] sunnyProbs = new double[states] { 0.5, 0.5, 0, 0 };
+    [SerializeField]
     private double[] cloudyProbs = new double[states] { 0.4, 0.2, 0.2, 0.2 };
+    [SerializeField]
     private double[] rainingProbs = new double[states] { 0.5, 0.1, 0.1, 0.3 };
+    [SerializeField]
     private double[] freezingProbs = new double[states] { 0.4, 0.1, 0.2, 0.3 };
 
-    // Use this for initialization
-    void Start()
+    float time = 0;
+
+    private void Start()
     {
         transitions[0] = sunnyTrans;
         transitions[1] = cloudyTrans;
         transitions[2] = rainingTrans;
         transitions[3] = freezingTrans;
 
+        UpdateProbabilities();
+
+        markovChain = new MarkovChain();
+    }
+
+    private void UpdateProbabilities()
+    {
         probabilities[0] = sunnyProbs;
         probabilities[1] = cloudyProbs;
         probabilities[2] = rainingProbs;
         probabilities[3] = freezingProbs;
+        tempProbabilities = probabilities;
+    }
 
-        markovChain = new MarkovChain();
-
-        InvokeRepeating("CalculateWeatherState", startTime, repeatRateTime);
+    private void Update()
+    {
+        if (time < waitTime)
+            time += Time.deltaTime;
+        if(time > waitTime)
+        {
+            time = 0;
+            CalculateWeatherState();
+        }
     }
 
     private void CalculateWeatherState()
     {
+        if (tempProbabilities != probabilities)
+        {
+            Debug.Log("CHANGED");
+            UpdateProbabilities();
+        }
+
         currentState = markovChain.CalculateWeatherState(states, transitions, probabilities);
         runs++;
         Debug.Log("Run: " + runs + " State: " + currentState);
